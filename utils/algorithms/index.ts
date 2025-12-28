221579a0e90dfbbc6fdf045ef03a76c6f8cc8a04
MaryaAkhmed <MaryaAkhmed@stud.kpfu.ru>
2025-12-28 21:46:54 +0300

    Added algorithms module for distance calculations, route optimization, and place recommendations

---
+++ utils/algorithms/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Algorithms module
+ * Central export point for all algorithm utilities
+ */
+
+// Distance algorithms
+export {
+  calculateDistance,
+  calculateTravelTime,
+  calculateDistanceMatrix,
+  findNearestPoint,
+  type Coordinates,
+} from "./distance";
+
+// Route optimization algorithms
+export {
+  nearestNeighborRoute,
+  optimizeRouteFromLocation,
+  twoOptOptimization,
+  hybridRouteOptimization,
+  type PlaceWithLocation,
+  type OptimizedRoute,
+} from "./routeOptimization";
+
+// Place recommendation algorithms
+export {
+  recommendPlaces,
+  filterPlacesByConstraints,
+  getRecommendedPlaces,
+  type PlaceScore,
+} from "./placeRecommendation";
+
